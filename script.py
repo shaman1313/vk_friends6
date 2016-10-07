626525e1d27090ea90a1a3c467756a0099d14b8d
Alex Sham <kaifatdd@gmail.com>
2016-10-07 11:45:57 +0300

    working! great!

--- script.py
+++ script.py
@@ -19,6 +19,12 @@ whoIsSome='alesbelarus'       #ID or short adress of second person
 #19036902 not friend but 2 mutual fr
 
 
+
+
+#######################################################################################################
+#
+#               Defining of functions
+#
 #############################################################   function for convert short adress in ID
 # This function include the error checking for unknown adress or ID
 # Function return the string with ID
@@ -54,12 +60,7 @@ def isIdAndConvert (whoIs):
             error_msg_is=node.childNodes[0].nodeValue
             print ("Err with msg:", error_msg_is)
 
-            
 #############################################################################################################
-firstP_id=isIdAndConvert(whoAmI)
-print (firstP_id)
-secondP_id=isIdAndConvert(whoIsSome)
-print (secondP_id)
 
 ################################################################## Function for getting friendlist for user ID
 # Function return the list with ID of user`s friends as members of list
@@ -80,6 +81,42 @@ def getListOfFriends (person_id):
     #print("\n", listOfFriends_1P)
     return listOfFriends
 ###############################################################################################################
+
+############################################################################ Function to form first half of chain
+
+def searchMatchR(list1, list2, result, count):
+    for a in list1:
+        for b in list2:
+            if a == b:
+                result.append([])
+                result[count].append(firstP_id)
+                result[count].append(a)
+                
+                return 0
+    return 1
+
+########################################################################### Function to form second half of chain
+def searchMatchR_2(list1, list2, result, count):
+    for a in list1:
+        for b in list2:
+            if a == b:
+                result[count].append(b)
+                result[count].append(secondP_id)
+                
+                return 0
+    return 1
+#################################################################################################################
+#################################################################################################################
+#
+#                   Block of code
+#
+#################################################################################################################
+
+firstP_id=isIdAndConvert(whoAmI)
+print (firstP_id)
+secondP_id=isIdAndConvert(whoIsSome)
+print (secondP_id)
+
 
 listOfFr_1p_lay1=getListOfFriends(firstP_id)
 print("In layer 1 are %d entries for P1" % len(listOfFr_1p_lay1))
@@ -126,79 +163,75 @@ if len(list(mn_betw)) == 0:
             print('You haven`t mutual friends in 2st layer')
         else:
             print ('You have %d mutual friends' % len(list(mn_betw22)))
-            print (list(mn_betw22))
+            #print (list(mn_betw22))
             #sys.exit()          ######## Exit if Match results
     
                 
     else:
         print ('You have %d mutual friends' % len(list(mn_betw21)))
-        print (list(mn_betw21))
+        #print (list(mn_betw21))
         sys.exit()          ######## Exit if Match results
 
 else:
     print ('You have %d mutual friends' % len(list(mn_betw)))
-    print (list(mn_betw))
+    #print (list(mn_betw))
     sys.exit()          ######## Exit if Match results
 ##################################################################################################################
-
+print('Building the chains...')
 contact=list(mn_betw22)
-'''
-
-listOfFr_1p_lay1
-listOfFr_2p_lay1
-
-'''
-########################################################
-
-def searchMatchR(list1, list2, result, count):
-    for a in list1:
-        for b in list2:
-            if a == b:
-                result.append([])
-                result[count].append(firstP_id)
-                result[count].append(a)
-                return 0
-    return 1
-
-########################################################
-def searchMatchR_2(list1, list2, result, count):
-    for a in list1:
-        for b in list2:
-            if a == b:
-                result[count].append(b)
-                result[count].append(secondP_id)
-                return 0
-    return 1
-########################################################
 connection_table=[]
 count=0
+jj=0
 for mem in contact:
     cont_frlst=getListOfFriends (mem)
     suc_f=searchMatchR(cont_frlst, listOfFr_1p_lay1, connection_table, count)
     if suc_f==0:
         connection_table[count].append(mem)
         searchMatchR_2(cont_frlst, listOfFr_2p_lay1, connection_table, count)
-    count+=1 
-        
-
-print (connection_table)
+        count+=1
+        continue
+    jj+=1
+    sys.stdout.write("\rdone %.2f persent" % ((jj/len(contact))*100))
 
 #############################################################
-'''
-Traceback (most recent call last):
-  File "C:\Users\Admin\Desktop\programming\Python\friends\script.py", line 177, in <module>
-    suc_f=searchMatchR(cont_frlst, listOfFr_1p_lay1, connection_table, count)
-  File "C:\Users\Admin\Desktop\programming\Python\friends\script.py", line 158, in searchMatchR
-    result[count].append(firstP_id)
-IndexError: list index out of range
 
-'''
+print ('\n')
 
+#######################################################################func user data
+def getUserData(person_id):
 
+    person=urllib.request.urlopen("https://api.vk.com/method/users.get.xml?user_id=%d" % int(person_id))
+    dec_person=person.read().decode("utf-8")
 
+    f=open("person.xml", 'w', encoding='utf-8')
+    f.write(dec_person)
+    f.close()
 
+    xml=parse('person.xml')
+    uid=xml.getElementsByTagName('uid')
+    for node in uid:
+        uid_v=(node.childNodes[0].nodeValue)
+    first_name=xml.getElementsByTagName('first_name')
+    for node in first_name:
+        first_name_v=(node.childNodes[0].nodeValue)
+    last_name=xml.getElementsByTagName('last_name')
+    for node in last_name:
+        last_name_v=(node.childNodes[0].nodeValue)
 
-
+    first_name_v=first_name_v.replace('\u0456', 'i')
+    last_name_v=last_name_v.replace('\u0456', 'i')
+    
+    sys.stdout.write('%s %s' % (first_name_v, last_name_v))
+    return 0
+########################################################################
+gi=0
+for mem in connection_table:
+    
+    for user in connection_table[gi]:
+        getUserData(user)
+        sys.stdout.write(' -> ')
+    gi+=1
+    sys.stdout.write('\n')
 
 
 
