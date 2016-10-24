b4283a72772e893d4ce60ea15444fb24a0916e4e
Alex Sham <kaifatdd@gmail.com>
2016-10-24 15:41:22 +0300

    Added empty progress window

--- modules/window.py
+++ modules/window.py
@@ -3,8 +3,48 @@ sys.path.append(".\\modules")
 from script import *
 
 from PyQt5.QtWidgets import (QWidget, QPushButton, QLineEdit,
-    QInputDialog, QApplication, QLabel, QHBoxLayout, QVBoxLayout)
-from PyQt5.QtCore import pyqtSignal, QObject
+    QInputDialog, QApplication, QLabel, QHBoxLayout, QVBoxLayout, QProgressBar)
+from PyQt5.QtCore import pyqtSignal, QObject, QPoint
+    
+
+
+class ProgressWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initProgressWindow()
+
+
+    def initProgressWindow(self):
+        cancelButton = QPushButton("Cancel")
+        cancelButton.clicked.connect(QApplication.instance().quit)
+        hboxManage = QHBoxLayout()
+        hboxManage.addStretch(1)
+        hboxManage.addWidget(cancelButton)
+        
+        progress=QProgressBar()
+        hboxProgress=QHBoxLayout()
+        #hboxProgress.addStretch(1)
+        hboxProgress.addWidget(progress)
+        
+        self.label=QLabel("In progress:")
+        self.hboxDescription=QHBoxLayout()
+        self.hboxDescription.addWidget(self.label)
+        
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(self.hboxDescription)
+        vbox.addStretch(1)
+        vbox.addLayout(hboxProgress)
+        vbox.addStretch(1)
+        vbox.addLayout(hboxManage)
+        self.setLayout(vbox)    
+        self.setGeometry(300, 300, 400, 250)
+        self.setMinimumSize(400, 250)
+        self.setMaximumSize(400, 250)
+        self.setWindowTitle('In progress...')    
+        self.show()
+
     
     
     
@@ -12,7 +52,7 @@ class FirstWindow(QWidget):
 
     def __init__(self):
         super().__init__()
-
+        self.secondWin = None
         self.initUI()
 
 
@@ -25,6 +65,7 @@ class FirstWindow(QWidget):
         okButton = QPushButton("OK")
         cancelButton = QPushButton("Cancel")
         
+        self.label1=QLabel('Enter the ID.\nPress the "Add" button and enter the ID or short adress of VK user\nThen press "OK"')
         self.line1=QLineEdit("First ID")
         self.line2=QLineEdit("Second ID")
         self.line1.setReadOnly(True)
@@ -40,6 +81,9 @@ class FirstWindow(QWidget):
         hboxManage.addWidget(okButton)
         hboxManage.addWidget(cancelButton)
         
+        hboxTheLabel=QHBoxLayout()
+        hboxTheLabel.addWidget(self.label1)
+        
         hboxFirstLine=QHBoxLayout()
         hboxFirstLine.addWidget(self.line1)
         hboxFirstLine.addWidget(addButton1)
@@ -48,12 +92,17 @@ class FirstWindow(QWidget):
         hboxSecondLine.addWidget(self.line2)
         hboxSecondLine.addWidget(addButton2)
         
-        vboxMainArea=QVBoxLayout()
+        vboxDescription=QVBoxLayout()
+        vboxDescription.addLayout(hboxTheLabel)
         
+        
+        vboxMainArea=QVBoxLayout()
         vboxMainArea.addLayout(hboxFirstLine)
         vboxMainArea.addLayout(hboxSecondLine)
         
         vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(vboxDescription)
         vbox.addStretch(1)
         vbox.addLayout(vboxMainArea)
         vbox.addStretch(1)
@@ -62,7 +111,9 @@ class FirstWindow(QWidget):
         
         self.setLayout(vbox)    
         
-        self.setGeometry(300, 300, 400, 150)
+        self.setGeometry(300, 300, 400, 250)
+        self.setMinimumSize(400, 250)
+        self.setMaximumSize(400, 250)
         self.setWindowTitle('Let`s start!')    
         self.show()
         
@@ -88,22 +139,19 @@ class FirstWindow(QWidget):
             
             
     def OK(self):
-        main(self.line1_val, self.line2_val)
-
-
-'''
-class ProgressWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        self.initUI()
-
-
-    def initUI(self):
+        
+        self.close()
+        if not self.secondWin:
+            self.secondWin=ProgressWindow()
+        #main(self.line1_val, self.line2_val)
+        
     
-    '''    
+
+
 
 def GUI():
     app = QApplication(sys.argv)
+    
     ex = FirstWindow()
+    
     sys.exit(app.exec_())
